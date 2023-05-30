1f7217da5b5898e806bdfdfc072df32896ccd6b8
simretB05 <happysimret@gmail.com>
2023-05-29 18:02:12 -0600

    added a function that looks for the large number

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+# Write a function that takes 1 number as an arugment and returns that number squared
 def squareNumbers(num):
    return num*num
 sqNumber=squareNumbers(8)
 print(sqNumber)
+# Write a function that takes 3 numbers as arugments as returns the largest one.
+def largeNumber(num_one,num_two,num_three):
+   if(num_one>num_two and num_one >num_three):
+    return  num_one
+   elif(num_two>num_one and num_two>num_three):
+     return num_two
+   else:
+     return num_three
+largeNum= largeNumber(100,700,900)
+print(largeNum)
+
 
